30a12cb8c49dea0f42a1ea448f54ea212b6cd8d4
athulyasaji05 <athulyasai06@gmail.com>
2024-06-10 21:45:34 +0530

    commit

---
+++ stringmodification.py
@@ -0,0 +1,14 @@
+a="Hello"
+print(a.upper())
+print(a.lower())
+a="Athu"
+print(a.replace("A","a"))
+b="lya"
+c=a+b
+print(c)
+z="Hello World"
+print(z.split(","))
+#formatting
+name="Ammu"
+txt=f"My name is {name}"
+print(txt)
