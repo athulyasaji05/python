59f72e812cb18954d9f62d7807496dc4e6daee52
athulyasaji05 <athulyasai06@gmail.com>
2024-06-11 22:17:47 +0530

    commit

--- conditions.py
+++ conditions.py
@@ -1,5 +1,10 @@
 a=20
 if a>50:
    print("a is greater than 50")
+print("a is lesser than 50")
+    
+b=20
+if b%2==0:
+    print("b is an even number")
 else:
-    print("a is lesser than 50")
+    print("b is an odd number")
