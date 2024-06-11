2a9484e8c42625dc5eb1491482d35d764bdd25a6
athulyasaji05 <athulyasai06@gmail.com>
2024-06-11 22:25:19 +0530

    commit

--- conditions.py
+++ conditions.py
@@ -8,3 +8,26 @@ if b%2==0:
     print("b is an even number")
 else:
     print("b is an odd number")
+    
+c=21
+if c<25:
+    if c%2==0:
+        print("c is an even number and less than 25")
+    else:
+        print("c is an odd number and less than 25")
+else:
+    print("c is greater than 25")
+    
+var="z"
+if var=="a":
+    print("vowel")
+elif var=="e":
+   print("vowel")
+elif var=="i":
+   print("vowel")
+elif var=="o":
+   print("vowel")
+elif var=="u":
+   print("vowel")
+else:
+    print("consonent")
