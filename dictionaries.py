7ff58c1f78cc8263aa22251ea42e3925ae5f8b83
athulyasaji05 <athulyasai06@gmail.com>
2024-06-11 21:21:43 +0530

    committed

--- dictionaries.py
+++ dictionaries.py
@@ -3,3 +3,11 @@ print(d1)
 print(type(d1))
 d2={1:"Hello",2:"World"}
 print(d2)
+d3={"name":"athu"}
+print(d3)
+d4=dict({1:"Hello"})
+print(d4)
+d5=dict([(1,"welcome")])
+print(d5)
+d6={"name":{"fname":"Athulya","lname":"Saji"}}
+print(d6)
