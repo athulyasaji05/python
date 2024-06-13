6049646c39826c126d2bdab4071a928633b0a2d0
athulyasaji05 <athulyasai06@gmail.com>
2024-06-13 21:37:44 +0530

    committed

--- oops.py
+++ oops.py
@@ -10,3 +10,28 @@ class Person:
 p1=Person('athu',22)
 print(p1.name)
 print(p1.age)
+
+#print(p1)
+
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def __str__(self):
+        return f"{self.name},{self.age}"
+        
+p1=Person('athu',22)
+print(p1)
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def myfun(self):
+        print("My name is " + self.name)
+
+p1 = Person('athu', 22)
+p1.myfun()
+
+      
