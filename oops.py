97f92425c2b9602a86cc9f13f39136a09406383c
athulyasaji05 <athulyasai06@gmail.com>
2024-06-13 21:16:42 +0530

    committed

--- oops.py
+++ oops.py
@@ -2,3 +2,11 @@ class MyClass:
     x=5
 print(MyClass)
 print(MyClass.x)
+
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+p1=Person('athu',22)
+print(p1.name)
+print(p1.age)
